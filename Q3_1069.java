ab39af57d64426a2049707407f190219604d6606
Shahid-codr7 <147031808+Shahid-codr7@users.noreply.github.com>
2023-10-21 21:21:45 +0530

    Update Q3_1069.java

--- Q3_1069.java
+++ Q3_1069.java
@@ -1,3 +1,7 @@
+/* NAME: SHAHID ABDUL FARHAD
+ * REGD. NO.: 2341006002
+ * PS LINK:  https://cses.fi/problemset/task/1069
+ */ 
 package CodexStart_2341006002;
 import java.util.*;
 public class Q3_1069 
